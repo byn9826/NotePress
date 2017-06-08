cb7db9821efab4726ce98165cb0f0fdbb60b685a
byn9826 <byn9826@gmail.com>
2017-06-07 22:10:06 -0400

    default content

--- footer.php
+++ footer.php
@@ -11,16 +11,14 @@
 
 ?>
 
-	</div><!-- #content -->
+	</div>
 
-	<footer id="colophon" class="site-footer" role="contentinfo">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'notepress' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'notepress' ), 'WordPress' ); ?></a>
-			<span class="sep"> | </span>
-			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'notepress' ), 'notepress', '<a href="https://automattic.com/" rel="designer">Underscores.me</a>' ); ?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
-</div><!-- #page -->
+	<footer id="footer">
+		<a href="https://github.com/byn9826/Notepress" target="_blank"><h5>
+			WordPress Theme: NotePress
+		</h5></a>
+	</footer>
+</div>
 
 <?php wp_footer(); ?>
 
--- index.php
+++ index.php
@@ -27,7 +27,7 @@ get_sidebar(); ?>
 				echo '<div class="main-list" >';
 					echo '<h3>' . $l -> post_title . '</h3>';
 					echo '<h5 class="main-list-date">' . substr($l -> post_date, 0, 10) . '</h5>';
-					echo '<h4>' . wp_trim_words( $l->post_content, 16, ' ...' ) . '</h4>';
+					echo '<h4>' . wp_trim_words( $l-> post_content, 16, ' ...' ) . '</h4>';
 				echo '</div>';
 			}
 			
@@ -35,46 +35,35 @@ get_sidebar(); ?>
 	</main>
 
 	<aside id="one">
+		<?php
+		$args = [ 
+			'category' => 1,
+			'numberposts' => 1,
+			'post_type' => 'post'
+		];
+		$recent = wp_get_recent_posts($args);
+		
+		$categories = wp_get_post_categories($recent[0]['ID']);
+		$tags = wp_get_post_tags($recent[0]['ID']);
+		//var_dump($recent);
+		echo '<h1>' . $recent[0]['post_title'] . '</h1>';
+		
+		foreach ($categories as $c) {
+			$note = get_category($c);
+    	echo '<h5 class="one-book">' . $note -> name . '</h5>';
+		}
+		
+		foreach ($tags as $t) {
+    	echo '<h5 class="one-tag">' . $t -> name . '</h5>';
+		}
+		
+		echo '<h5 id="one-time">' . substr($recent[0][post_date], 0, 10) . '</h5>';
+		echo '<article id="one-content">' . $recent[0][post_content] . '</article>';
+			
+		?>
+		
 	</aside>
 
 <?php
 
 get_footer();
-
-
-
-
-		/*
-		if ( have_posts() ) :
-
-			if ( is_home() && ! is_front_page() ) : ?>
-				<header>
-					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
-				</header>
-
-			<?php
-			endif;
-
-			/* Start the Loop */
-			/*
-			while ( have_posts() ) : the_post();
-
-				/*
-				 * Include the Post-Format-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-				 */
-		/*
-				get_template_part( 'template-parts/content', get_post_format() );
-
-			endwhile;
-
-			the_posts_navigation();
-
-		else :
-
-			get_template_part( 'template-parts/content', 'none' );
-
-		endif; 
-		*/
-		?>
